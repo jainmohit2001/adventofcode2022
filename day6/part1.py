32bd03a16e51aec667113a11b387bb3c75315095
jainmohit2001 <jainmohit2001@gmail.com>
2023-11-29 14:53:49 +0530

    completed day 6

---
+++ day6/part1.py
@@ -0,0 +1,27 @@
+def process_signal(signal: str):
+    for i in range(0, len(signal) - 4):
+        chars = {}
+        duplicate = False
+
+        for j in range(0, 4):
+            if signal[i + j] in chars:
+                duplicate = True
+                break
+            chars[signal[i + j]] = True
+
+        if not duplicate:
+            print(i + j + 1)
+            return
+
+
+def main():
+    # Read input
+    with open("input.txt", "r", encoding="utf8") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        process_signal(line.strip())
+
+
+if __name__ == "__main__":
+    main()
